aa3cee5fba36762f931a101c591efb96b7951121
Dazed31 <darrenamore13@gmail.com>
2026-06-09 23:43:14 +0300

    Created the Course class

---
+++ models/course.py
@@ -0,0 +1,12 @@
+class Course:
+    def __init__(self, course_id, course_name, trainer_name, capacity):
+        self.course_id = course_id
+        self.course_name = course_name
+        self.trainer_name = trainer_name
+        self.capacity = capacity
+
+    def display(self):
+        print(f"Course ID: {self.course_id}")
+        print(f"Course Name: {self.course_name}")
+        print(f"Trainer: {self.trainer_name}")
+        print(f"Capacity: {self.capacity}")
