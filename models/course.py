8c5c7d1ed2da5e2f2f10700d4fbd38df916c049e
Dazed31 <darrenamore13@gmail.com>
2026-06-10 00:36:38 +0300

    Add Student and Course model classes

--- models/course.py
+++ models/course.py
@@ -3,10 +3,10 @@ class Course:
         self.course_id = course_id
         self.course_name = course_name
         self.trainer_name = trainer_name
-        self.capacity = int(capacity)
+        self.capacity = capacity
 
     def display(self):
         print(f"Course ID: {self.course_id}")
         print(f"Course Name: {self.course_name}")
         print(f"Trainer: {self.trainer_name}")
-        print(f"Capacity: {self.capacity}")
+        print(f"Capacity: {self.capacity} students")
--- models/student.py
+++ models/student.py
@@ -1,10 +1,9 @@
-from models.person import Person
-
-
-class Student(Person):
+class Student:
     def __init__(self, student_id, name, email, phone_number):
-        super().__init__(name, email, phone_number)
         self.student_id = student_id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
 
     def display(self):
         print(f"Student ID: {self.student_id}")
